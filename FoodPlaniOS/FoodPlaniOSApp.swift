77937414b73dd071bb065868db0d5a9c807d249c
Ali <alisaleem877@gmail.com>
2021-12-12 19:21:14 +0500

    Initial Commit

---
+++ FoodPlaniOS/FoodPlaniOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPlaniOSApp.swift
+//  FoodPlaniOS
+//
+//  Created by Muhammad Ali on 12/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodPlaniOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
